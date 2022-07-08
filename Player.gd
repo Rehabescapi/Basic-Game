0b808d69126647ebc0c1083980b2c1332688d6bc
Rehabescapi <jasonmlehmann@gmail.com>
2022-07-08 14:02:25 -0500

    Added Basic Game Fuctions

--- Player.gd
+++ Player.gd
@@ -21,4 +21,9 @@ func get_input():
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y +=1
+		
 	velocity = velocity.normalized() * speed
